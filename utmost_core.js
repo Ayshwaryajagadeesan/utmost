d1f925503e3ab2b12ccd4569770563de873bbde9
Jared Karlow <ituralde@umich.edu>
2014-12-18 12:56:21 +0000

    git-svn-id: file:///srv/svn/cmisst/projects/utmost/trunk@1437 008e0eca-ecc9-4b3e-88f1-516806e67678

--- utmost_core.js
+++ utmost_core.js
@@ -147,18 +147,18 @@ function utmost_core(returning_user){
 	// Chart Code
 	Ext.define('Crash_Type_Count', {
 		extend: 'Ext.data.Model',
-		fields: ['crash_count', 'crash_type']
+		fields: ['crash_count', 'crash_type', 'crash_count_fake']
 	});
 	
 	
 	var utmost_chart_values = Ext.create('Ext.data.Store', {
 		model: 'Crash_Type_Count',
 		data : [
-			{crash_count: "100", crash_type: "Rear End"},
-			{crash_count: "50", crash_type: "Frontal"},
-			{crash_count: "45", crash_type: "Rollover"},
-			{crash_count: "37", crash_type: "Side Impact"},
-			{crash_count: "64", crash_type: "Pedestrian"}
+			{crash_count: "100", crash_type: "Rear End", crash_count_fake: "75"},
+			{crash_count: "50", crash_type: "Frontal", crash_count_fake: "15"},
+			{crash_count: "45", crash_type: "Rollover", crash_count_fake: "25"},
+			{crash_count: "37", crash_type: "Side Impact", crash_count_fake: "13"},
+			{crash_count: "64", crash_type: "Pedestrian", crash_count_fake: "57"}
 		]
 	});
 	
@@ -205,6 +205,31 @@ function utmost_core(returning_user){
 				},
 				xField: 'crash_type',
 				yField: 'crash_count'
+			},{
+				type: 'column',
+				axis: 'left',
+				highlight: true,
+				tips: {
+				  trackMouse: true,
+				  width: 140,
+				  height: 28,
+				  renderer: function(storeItem, item) {
+					this.setTitle(storeItem.get('crash_type') + ': ' + storeItem.get('crash_count_fake'));
+				  }
+				},
+				style: {
+                    fill: '#FFD119'
+                },
+				label: {
+				  display: 'insideEnd',
+				  'text-anchor': 'middle',
+					field: 'data',
+					renderer: Ext.util.Format.numberRenderer('0'),
+					orientation: 'vertical',
+					color: '#333'
+				},
+				xField: 'crash_type',
+				yField: 'crash_count_fake'
 			}
 		]
 	});
