d38dab8b3d45d23fa011b92c9bddca61e66ba5f7
Jared Karlow <ituralde@umich.edu>
2014-12-18 12:24:17 +0000

    git-svn-id: file:///srv/svn/cmisst/projects/utmost/trunk@1430 008e0eca-ecc9-4b3e-88f1-516806e67678

--- utmost.php
+++ utmost.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 	<head>
-		<script type "text/javascript" src="lib/ext-all.js"></script>
+		<script type "text/javascript" src="lib/ext-all-debug.js"></script>
 		<!-- <script type="text/javascript" src="utmost_model.js"></script>
 		<script type="text/javascript" src="utmost_chart.js"></script>
 		<script type="text/javascript" src="utmost_countermeasure.js"></script> -->
