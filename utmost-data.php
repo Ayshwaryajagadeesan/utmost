910adfbd2c4102194bf341ecef347f53e5f25849
Jared Karlow <ituralde@umich.edu>
2015-03-05 13:50:38 +0000

    git-svn-id: file:///srv/svn/cmisst/projects/utmost/trunk@1468 008e0eca-ecc9-4b3e-88f1-516806e67678

---
+++ utmost-data.php
@@ -0,0 +1,34 @@
+<?php>
+
+//	SELECT distinct crash_type, sum(Frequency) as crash_count, sum(Frequency *(1-(0))) as crash_count_adj FROM `utmost_data` WHERE 1  GROUP BY crash_type
+
+// (0 value replaced by set of cms enabled)
+		$filters = $_GET["filter-string"];
+		$coeffs = $_GET["coeffs-string"];
+		$filter_array = explode('~', $filters);
+		$coeff_array = explode('~', $filters);
+		$utmost_link= mysql_connect('cmisst-live-db.miserver.it.umich.edu', 'mtcf-sys', 'a1s2d3f4');
+        mysql_select_db('UTMOST', $utmost_link);
+        $data = array();
+		$query = "SELECT distinct crash_type, sum(Frequency) as crash_count, sum(Frequency *(1-(0))) as crash_count_adj FROM `utmost_data` GROUP BY crash_type";
+		
+		$counter = 0;
+		$builder_array = array();
+		while ($counter < count($filter_array)){
+			$builder_array[] = "(".$filter_array[$counter]." * ".$coeff_array[$counter].")";
+			$counter++;
+		}
+		
+		if (count($filter_array) > 0){
+			$filter_query_string = implode(" + ", $builder_array);
+			$query = "SELECT distinct crash_type, sum(Frequency) as crash_count, sum(Frequency *(1-(0 + ".$filter_query_string."))) as crash_count_adj FROM `utmost_data` GROUP BY crash_type";
+		}
+       
+        $rs=mysql_query($query ,$utmost_link);
+        while ($row = mysql_fetch_assoc($rs)){
+                $data[] = $row;
+        }
+		
+		echo json_encode($data);
+
+?>
