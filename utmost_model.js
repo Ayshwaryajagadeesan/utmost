04eaa68d3daf8a4645397bcc6a297c7825a2657a
Jared Karlow <ituralde@umich.edu>
2015-10-12 20:57:21 +0000

    git-svn-id: file:///srv/svn/cmisst/projects/utmost/trunk@1532 008e0eca-ecc9-4b3e-88f1-516806e67678

--- utmost_model.js
+++ utmost_model.js
@@ -79,7 +79,7 @@
 				}
 				utmost_totals_chart_values.getAt(0).set('crash_count', total);
 				utmost_totals_chart_values.getAt(0).set('crash_count_adj', adj_total);
-				utmost_chart.axes.get(0).maximum = max;
+				utmost_chart.axes.get(0).maximum = max * 1.25;
 			}
 		});
 		
